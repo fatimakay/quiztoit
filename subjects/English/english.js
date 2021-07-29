2663edf6e13f4cf90fb230880dc43067cbfa0350
fatimakay <87754090+fatimakay@users.noreply.github.com>
2021-07-29 14:34:17 +0500

    Add files via upload

---
+++ subjects/English/english.js
@@ -0,0 +1,54 @@
+/*eslint-env es6*/
+const quiz = [   
+    {
+        q:'I told you ______ you did not listen.',
+        options:['because', 'but', 'for','then'],
+        answer:1
+    },
+    {
+        q: 'Is there a doctor in the house?',
+        options:['Yes, is there.','Yes, there is.', 'No, theirs not.', ' No, they is not.'],
+        answer: 1
+    },
+    {
+        q: 'George Washington ______ the first President of the United States.',
+        options:['were','will','was', 'where'],
+        answer: 2
+    },
+    {
+        q: 'Please, let me ______!',
+        options:['think','put', 'have','want'],
+        answer: 0
+    },
+    {
+        q: 'She ______ blue velvet to the party last night.',
+        options:['worn','wore', 'war', 'weared'],
+        answer: 1
+    }, 
+    {
+        q: "Paul can't touch the ceiling because he is too ______.", 
+        options: ['shortening', 'shortness', 'shorten', 'short'],
+        answer: 3
+    },
+    {
+        q: "Monica is tired. She doesn't want to go ______ school.", 
+        options: ['at', 'for', 'to', 'in'],
+        answer: 2
+    },
+        {
+        q: "The wind ______ hard all night.", 
+        options: ['blow', 'blew', 'blown', 'blowed'],
+        answer: 1
+    },
+        {
+        q: "We get good grades ______ we study.", 
+        options: ['but', 'except', 'during', 'because'],
+        answer: 3
+    },
+        {
+        q: "I'm sorry, you will just have to ______.", 
+        options: ['wait', 'avoid', 'like', 'attempt'],
+        answer: 0
+    }
+        
+]
