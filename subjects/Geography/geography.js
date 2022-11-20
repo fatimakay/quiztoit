ed4810df04f5a5c3ed1fe2e2ff0b3a3b50d53acd
Fatima Saeed Khan <fatima.k215@gmail.com>
2022-11-20 18:07:01 +0500

    revamped

---
+++ subjects/Geography/geography.js
@@ -0,0 +1,54 @@
+/*eslint-env es6*/
+const quiz = [   
+    {
+        q:'Q 1 - Which one of the following countries experiences a maximum volcanic eruption?',
+        options:['Japan', 'Italy', 'Philippines','Fiji'],
+        answer:0
+    },
+    {
+        q: 'What is Casuarina?',
+        options:['An animal species','A tribe of South Africa', ' A Rhododendron', 'A tree species'],
+        answer: 3
+    },
+    {
+        q: 'Keibul Lamjao, the floating national park, is located in ________',
+        options:['Brazil','China','Japan', 'India'],
+        answer: 3
+    },
+    {
+        q: 'What is the meaning of "culling"?',
+        options:['Killing or segregating animals from a breeding stock','Introducing a new species in an ecological system', 'Making special provision to protects endangered species','Re-introduction of extinct species.'],
+        answer: 0
+    },
+    {
+        q: 'Who did first use the term ‘Big Bang?’',
+        options:['Amo Penzias','Robert Wilson', 'Fred Hoyle', 'Laplace'],
+        answer: 2
+    }, 
+    {
+        q: "Which one of the following is an insectivorous plant?", 
+        options: ['Nelumbo nucifera', 'Cypripedioideae', 'Rhynchostylis gigantean', 'Pinguicula'],
+        answer: 3
+    },
+    {
+        q: "The longest river of South America and the second longest river of the world Amazon debouches into ________", 
+        options: ['Pacific Ocean', 'Atlantic Ocean', 'Indian Ocean', 'Gulf of Mexico'],
+        answer: 1
+    },
+        {
+        q: "_______ % is the net shown area of the total geographical land of India.", 
+        options: ['33', '56', '46', '65'],
+        answer: 2
+    },
+        {
+        q: "Geoponic is related to ………………", 
+        options: ['Cultivation of plant by using nutrient and water', 'Cultivation of plant without using water', ' Growing plants in a normal soil', 'Cultivation of plant without using sunlight'],
+        answer: 2
+    },
+        {
+        q: "Which one of the following is not a Biosphere Reserves?", 
+        options: ['Nanda Devi', 'Gulf of Mannar', 'Pachmarhi', 'Bandipur'],
+        answer: 3
+    }
+        
+]
